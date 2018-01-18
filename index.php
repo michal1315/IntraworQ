e823085bf2727aac9d9e941ee628bc377f617ba0
michal1315 <35575000+michal1315@users.noreply.github.com>
2018-01-18 22:27:44 +0100

    Initial state of project
    create html,css,js,php,

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Zadanie testowe</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="libs/jquery-min.js"></script>
+    <script src="scripts/script.js"></script>
+</head>
+<body>
+
+<div class="container">
+
+    <div class="up"></div>
+    <div class="left"></div>
+    <div class="center"></div>
+    <div class="right"></div>
+    <div class="footer"></div>
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+</body>
+</html>
